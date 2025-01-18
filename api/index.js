5e1922dc075cdebc8b40f5499de7a0b45175c414
vinod-kumar1 <vinu22149@gmail.com>
2025-01-19 04:52:44 +0530

    added createuser endpoint

--- api/index.js
+++ api/index.js
@@ -87,7 +87,7 @@ app.post("/createuser", async (req, res) => {
     let data = await new user({ sessionkey: hashKey, email: email }).save();
     res.status(201).send(`Here are your details: ${data}`);
   } catch (err) {
-    res.status(500).send("Please try again, the issue is");
+    res.status(500).send(`Please try again, the issue is : ${err}`);
   }
 });
 
